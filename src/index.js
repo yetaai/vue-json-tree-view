0a3b1de52a0f9eeaf063ec3b51f0f7ebd4cf0fe7
oefish <dj@bigdecimap.com>
2019-01-24 16:20:52 -0800

    1.0.3 Babel trial

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import TreeViewItemValue from './TreeViewItemValue.vue';
 import TreeViewItem from './TreeViewItem.vue';
 import TreeView from './TreeView.vue';
 
-module.exports = function install(Vue){
-    Vue.component("tree-view-item-value", TreeViewItemValue);
-    Vue.component("tree-view-item", TreeViewItem);
-    Vue.component("tree-view", TreeView);  
-};
+// module.exports = function install(Vue){
+//     Vue.component("tree-view-item-value", TreeViewItemValue);
+//     Vue.component("tree-view-item", TreeViewItem);
+//     Vue.component("tree-view", TreeView);
+// };
+export default {
+  TreeView,
+  TreeViewItem,
+  TreeViewItemValue
+}
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -6,7 +6,7 @@ var config = require('./webpack.base.config');
 
 config.entry = './src/index.js';
 config.output = {
-    filename: './dist/vue-json-tree-view.min.js',
+    filename: './dist/vueyytree.min.js',
     library: 'TreeView',
     libraryTarget: 'umd'
 };
