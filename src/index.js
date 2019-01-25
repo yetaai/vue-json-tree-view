1cb1c2480664061caa8760aa3bc42be82924b596
oefish <dj@bigdecimap.com>
2019-01-24 18:14:30 -0800

    Minor bug

--- src/index.js
+++ src/index.js
@@ -2,18 +2,8 @@ import TreeViewItemValue from './TreeViewItemValue.vue';
 import TreeViewItem from './TreeViewItem.vue';
 import TreeView from './TreeView.vue';
 
-// module.exports = function install(Vue){
-//     Vue.component("tree-view-item-value", TreeViewItemValue);
-//     Vue.component("tree-view-item", TreeViewItem);
-//     Vue.component("tree-view", TreeView);
-// };
-TreeView.install = function (Vue) {
-  Vue.component('TreeView', TreeView)
-}
-TreeViewItem.install = function (Vue) {
-  Vue.component('TreeViewItem', TreeViewItem)
-}
-TreeViewItemValue.install = function (Vue) {
-  Vue.component('TreeViewItemValue', TreeViewItemValue)
-}
-export default {TreeView, TreeViewItem, TreeViewItemValue}
+module.exports = function install(Vue){
+    Vue.component("tree-view-item-value", TreeViewItemValue);
+    Vue.component("tree-view-item", TreeViewItem);
+    Vue.component("tree-view", TreeView);
+};
